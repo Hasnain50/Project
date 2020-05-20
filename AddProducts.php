bbbe121c7bb7fda3268be45afd8c2d3ce4950729
Hasnain50 <hashmihasnain50@gmail.com>
2020-05-20 21:40:00 +0500

    dropdown not working

--- AddProducts.php
+++ AddProducts.php
@@ -6,12 +6,35 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+include 'connection.php';
+$query1="SELECT * from Category";
+$result1=mysqli_query($conn,$query1);
+
+
+?>
 <form method="POST">
 	<label>Enter Product Name : </label>
 	<input type="text" name="txtName"/>
 	<br>
 	<label>Enter Category</label>
 	<input type="text" name="txtCategory"/>
+	<select name="dCategory">
+			
+			<?php
+            if($result1)
+            {
+                while($row=mysqli_fetch_array($result1))
+                {
+                    //echo "<option value='".$row['C_Id']."'>".$row['C_Name']."</option>";
+            ?>
+                <option value="<?php echo $row['C_ID'];?>"><?php echo $row['C_NAME']?></option>
+            <?php
+                }
+            }
+            ?>
+        </select>
+        
 	<br>
 	<label>Enter Price</label>
 	<input type="text" name="txtPrice"/>
@@ -23,7 +46,7 @@
 </form>        
 
 <?php
-include 'connection.php';
+//include 'connection.php';
 if(isset($_POST['btnsubmit']))
 {
 $pName=$_POST['txtName'];
@@ -31,15 +54,15 @@ $pCategory=$_POST['txtCategory'];
 $pPrice=$_POST['txtPrice'];
 $pQuantity=$_POST['txtQuantity'];
 
-//echo "Name : ".$pName;
-//echo "Category : ".$pCategory;
 
 $query= "INSERT INTO Product (`P_NAME`,`P_CATEGORY`,`P_PRICE`,`P_QUANTITY`) VALUES
 ('$pName','$pCategory','$pPrice','$pQuantity')";
+
 $result=mysqli_query($conn,$query);
 if($result)
 {
-	echo "Data Inserted";
+	//echo "Data Inserted";
+
 }else{
 	echo "Error : ".mysqli_error($conn);
 }
